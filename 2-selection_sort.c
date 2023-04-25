f2aea2d48294c82cbfdab0b87ed7c1b85fac7877
Lord <yrnlehjend@gmail.com>
2023-04-25 20:16:38 +0000

    fix betty errors

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -42,7 +42,7 @@ void selection_sort(int *array, size_t size)
 			temp = array[min];
 			array[min] = array[i];
 			array[i] = temp;
-		   print_array(array, size);	
+		   print_array(array, size);
 		}
 	}
 }
