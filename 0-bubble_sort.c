fda9cdfe0a4337ee82a363da812f40890ea1312b
Lord <yrnlehjend@gmail.com>
2023-04-25 11:03:04 +0000

    add O notation

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,7 +21,7 @@ void bubble_sort(int *array, size_t size)
 	for (i = 0; i < size; i++)
 	{
 
-		for (j = 0; j < size - 1 - i; i++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			swap = 0;
 			if (array[j] > array[j + 1])
